1bbdd280e29c222a9b581e21839d05b513ce8713
Jack McPherson <jackemcpherson@gmail.com>
2026-09-17 23:32:46 +0000

    dotfiles v0.4.0

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -1,9 +1,9 @@
 class Dotfiles < Formula
   desc "Reconcile a Mac against the dotfiles bundle"
   homepage "https://github.com/jackemcpherson/dotfiles"
-  url "https://github.com/jackemcpherson/homebrew-tap/releases/download/v0.3.0/dotfiles-darwin-arm64"
-  sha256 "4d8f08fbcb7960b90f1ea27a9b10a8867bbc3af950a412adffdff2731410d34d"
-  version "0.3.0"
+  url "https://github.com/jackemcpherson/homebrew-tap/releases/download/v0.4.0/dotfiles-darwin-arm64"
+  sha256 "f060d8fddca29efcc0fd59d7b89b1782a929317527fa91ec67e98a0d62c7ad26"
+  version "0.4.0"
 
   depends_on :macos
   depends_on arch: :arm64
